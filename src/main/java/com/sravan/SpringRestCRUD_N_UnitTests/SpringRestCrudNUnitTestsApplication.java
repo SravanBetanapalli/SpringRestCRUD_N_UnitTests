a30cf1d63679b32d429a7d5e8bf964ae617766ef
Sravan Betanapalli(OSV) <sravan.betanapalli.osv@fedex.com>
2025-03-21 16:14:00 +0530

    rest code

---
+++ src/main/java/com/sravan/SpringRestCRUD_N_UnitTests/SpringRestCrudNUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.sravan.SpringRestCRUD_N_UnitTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestCrudNUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestCrudNUnitTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sravan/SpringRestCRUD_N_UnitTests/SpringRestCrudNUnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sravan.SpringRestCRUD_N_UnitTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestCrudNUnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
